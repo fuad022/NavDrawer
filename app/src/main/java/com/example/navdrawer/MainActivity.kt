8e258ed2a18c5ce9cf6cd6f0bcef0430c5b37e26
f.r.malikov <fuad.malikov@rabitabank.com>
2022-03-29 18:08:43 +0400

    NavDrawer demo app.

---
+++ app/src/main/java/com/example/navdrawer/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.navdrawer
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.navdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+
+            setSupportActionBar(mainToolbar)
+            val actionBarDrawerToggle = ActionBarDrawerToggle(
+                this@MainActivity,
+                drawerLayout,
+                mainToolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+            )
+
+            drawerLayout.addDrawerListener(actionBarDrawerToggle)
+            actionBarDrawerToggle.syncState()
+
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.folder -> {
+                        toast("Folder clicked")
+                        true
+                    }
+                    R.id.people -> {
+                        toast("People clicked")
+                        true
+                    }
+                    R.id.star -> {
+                        toast("Star clicked")
+                        true
+                    }
+                    R.id.access_time -> {
+                        toast("Access clicked")
+                        true
+                    }
+                    R.id.backup -> {
+                        toast("Backup clicked")
+                        true
+                    }
+                    R.id.settings_app -> {
+                        toast("Settings app clicked")
+                        true
+                    }
+                    R.id.power -> {
+                        toast("Power clicked")
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+        }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
